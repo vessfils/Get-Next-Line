dc37f368f011ab41a1d252ae7880a5da9f139d06
Jean-Francois <73600690+vessfils@users.noreply.github.com>
2021-12-07 19:19:59 +0100

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
